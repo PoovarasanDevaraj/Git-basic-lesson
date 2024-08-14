4f20ae2d28b93730ddeef62ea656dfaa51a59e10
poovarasandevaraj <poovarasandev20.02@gmail.com>
2024-08-14 16:29:59 +0530

    Initial Commit

---
+++ BasicGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicGit
+//
+//  Created by Poovarasan Devaraj on 14/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BasicGitTests/BasicGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicGitTests.swift
+//  BasicGitTests
+//
+//  Created by Poovarasan Devaraj on 14/08/24.
+//
+
+import Testing
+@testable import BasicGit
+
+struct BasicGitTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
